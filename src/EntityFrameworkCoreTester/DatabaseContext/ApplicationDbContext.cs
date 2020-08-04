0aa9fcacaf51a5f69ad32d018ae866c78cb1177b
eskye <sunkanmiijatuyi@gmail.com>
2020-08-04 11:32:44 +0100

    initial commit- project setup

---
+++ src/EntityFrameworkCoreTester/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityFrameworkCoreTester.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("Default"));
+        }
+    }
+}
---
+++ src/EntityFrameworkCoreTester/Model/Applicant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCoreTester.Model
+{
+   public class Applicant
+    {
+
+    }
+}
